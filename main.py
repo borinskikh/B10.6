a2e93ac5d51feed1b72e2c2fb6f3032fbd43718c
borinskikh <causelesscause@gmail.com>
2020-12-09 21:28:02 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import telebot
 
+token = "1420835702:AAGn-wPlzgNnqEnwSS4pzwlGF6t4xS4lAa8"
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def handleCommands(message):
+    bot.reply_to(message, '/start')
+
+
+@bot.message_handler(content_types=['text'])
+def replyToMessage(message):
+    bot.reply_to(message, "okay")
+
+
+bot.polling()
